9b194e130e80f1e6d106be908198d0a076128be8
Sharujan Sriramasothy <sharujan.sriramasothy@kcl.ac.uk>
2022-09-30 20:46:07 +0100

    Completed

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,14 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+
+    #adds the keys to the disctionary
+    for i in range(len(items)):
+        if frequencies.get(items[i]) is None:
+            frequencies[str(items[i])] = 0
+
+    #adds the values to the dictionary
+    for i in range (len(items)):
+        frequencies.update({str(items[i]): frequencies.get(str(items[i])) + 1})
+
     return frequencies
